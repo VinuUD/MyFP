acd05b591e306e568f8cee9db3d4e38170bb3a57
Vinu Dayaratne <vinudayaratne@gmail.com>
2020-05-25 04:10:48 +0530

    Added isPrime.scala to Assignment 3

---
+++ Assignment 3/Q1/isPrime.scala
@@ -0,0 +1,16 @@
+object isPrime extends App{
+    def prime(n:Int, x:Int):Boolean={
+        if(n == 0)
+            return true;
+
+        if(n == 1 || x == 1)
+            return true;
+        
+        if(n%x == 0)
+            return false;
+
+        prime(n, x-1);
+    }
+
+    printf("%b\n", prime(8, 8));
+}
